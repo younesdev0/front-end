4b3134bd1d96af464ce6bdd06fcb5768ea80853c
Younes Hidaoui <younesdev4@gmail.com>
2024-12-05 21:13:10 +0100

    v1.3

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -9,7 +9,7 @@
         <div class="statistics-grid">
           <StatCard v-if="totalSales" title="Total Sales" :value="totalSales"  suffix="$" />
 
-          <Cards v-if="topProduct?.length" :topProducts="topProduct" />
+          <Cards v-if="topProduct" :topProducts="topProduct" />
           <StatCard v-if="categoryCount" title="Sales Distribution by Category" :value="categoryCount" suffix="%" />
         </div>
         
@@ -42,23 +42,9 @@
 </template>
 
 <script lang="ts" setup>
-interface Product {
-  id: number;
-  name: string;
-  dateAdded: string;
-  price: number;
-  totalSales: number;
-}
 
-interface SalesData {
-  name: string;
-  sales: number;
-}
 
-interface CategorySalesData {
-  category: string;
-  sales: number;
-}
+
 import { ref, onMounted } from 'vue';
 import { useAnalyticsStore } from '@/stores/analytics';
 import { useProductStore } from '@/stores/product';
